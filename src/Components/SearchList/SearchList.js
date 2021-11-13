ce91a2c11d7943a4cf0a2e8129cab7a6e8b1b8b4
RomanVoznyy <roman.voznyy.52@gmail.com>
2021-11-13 23:16:48 +0200

    added search page

--- src/Components/SearchList/SearchList.js
+++ src/Components/SearchList/SearchList.js
@@ -1,7 +1,99 @@
+import { useState, useEffect } from 'react';
+import { NavLink } from 'react-router-dom';
+import { getSearchList } from '../../utils/apiservise';
+import { Pagination } from '@mui/material';
 import styles from './SearchList.module.css';
 
 const SearchList = () => {
-  return <h2 className={styles.title}>Hello list movies page</h2>;
+  const [request, setRequest] = useState('');
+  const [totalPages, setTotalPages] = useState(0);
+  const [page, setPage] = useState(1);
+  const [moviesList, setMoviesList] = useState([]);
+
+  useEffect(() => {
+    request &&
+      getSearchList({ query: request, page }).then(data => {
+        setMoviesList(data.results);
+      });
+  }, [page]);
+
+  const handleInput = ({ target }) => {
+    setRequest(target.value);
+  };
+
+  const handleSubmit = evt => {
+    evt.preventDefault();
+    setPage(1);
+    if (request) {
+      getSearchList({ query: request, page: 1 }).then(data => {
+        setTotalPages(data.total_pages);
+        setMoviesList(data.results);
+      });
+    } else {
+      console.log('Type something');
+      setTotalPages(0);
+      setMoviesList([]);
+    }
+  };
+
+  const handlePages = ({ target }) => {
+    setPage(Number(target.textContent));
+  };
+
+  return (
+    <section>
+      <h2 className={styles.title}>Hello list movies page</h2>
+      <form onSubmit={handleSubmit}>
+        <label className={styles.searchbar}>
+          <input type="text" value={request} onChange={handleInput} />
+        </label>
+        <button type="submit" className={styles.searchbar}>
+          ClickMe
+        </button>
+      </form>
+      {moviesList.length !== 0 && (
+        <>
+          <ul>
+            {moviesList.map(el => (
+              <li key={el.id}>
+                <NavLink to={`/movies/${el.id}`}>{el.title || el.name}</NavLink>
+              </li>
+            ))}
+          </ul>
+          <div className={styles.block}>
+            <Pagination
+              count={totalPages}
+              color="secondary"
+              page={page}
+              onChange={handlePages}
+            />
+          </div>
+        </>
+      )}
+    </section>
+  );
 };
 
 export default SearchList;
+
+// page: 1
+// results: [{…}]
+// total_pages: 1
+// total_results: 1
+// [[Prototype]]: Object
+
+// adult: false
+// backdrop_path: "/2va32apQP97gvUxaMnL5wYt4CRB.jpg"
+// genre_ids: (2) [14, 28]
+// id: 268
+// original_language: "en"
+// original_title: "Batman"
+// overview: "Batman must face his most ruthless nemesis when a deformed madman calling himself \"The Joker\" seizes control of Gotham's criminal underworld."
+// popularity: 42.593
+// poster_path: "/r7XF6duZy5ZXmOX7HE3fKGV1WLN.jpg"
+// release_date: "1989-06-23"
+// title: "Batman"
+// video: false
+// vote_average: 7.2
+// vote_count: 5844
+// [[Prototype]]: Object
--- src/utils/apiservise.js
+++ src/utils/apiservise.js
@@ -2,48 +2,44 @@ import axios from 'axios';
 const API_KEY = '6914e86918040074e2fe382ba8e8cb5e';
 const BASE_URL = 'https://api.themoviedb.org/3';
 
-const getTrendMovies = ({ page }) => {
-  return axios
-    .get(`${BASE_URL}/trending/all/day?api_key=${API_KEY}&page=${page}`)
-    .then(data => {
-      return data.data;
-    });
+const getTrendMovies = async ({ page }) => {
+  const data = await axios.get(
+    `${BASE_URL}/trending/all/day?api_key=${API_KEY}&page=${page}`,
+  );
+  return data.data;
 };
 
-const getSearchList = ({ query, page }) => {
-  return axios
-    .get(
-      `${BASE_URL}/search/movie?api_key=${API_KEY}&language=en-US&query=${query}&page=${page}&include_adult=false`,
-    )
-    .then(data => data.data);
+const getSearchList = async ({ query, page }) => {
+  console.log(query, page);
+  const data = await axios.get(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&language=en-US&query=${query}&page=${page}&include_adult=false`,
+  );
+  return data.data;
 };
 
-const getMovieInfo = ({ movie_id, type }) => {
-  return axios
-    .get(
-      `${BASE_URL}${
-        type === 'tv' ? '/tv' : '/movie'
-      }/${movie_id}?api_key=${API_KEY}&language=en-US`,
-    )
-    .then(data => data.data);
+const getMovieInfo = async ({ movie_id, type }) => {
+  const data = await axios.get(
+    `${BASE_URL}${
+      type === 'tv' ? '/tv' : '/movie'
+    }/${movie_id}?api_key=${API_KEY}&language=en-US`,
+  );
+  return data.data;
 };
-const getCastInfo = ({ movie_id, type }) => {
-  return axios
-    .get(
-      `${BASE_URL}${
-        type === 'tv' ? '/tv' : '/movie'
-      }/${movie_id}/credits?api_key=${API_KEY}&language=en-US`,
-    )
-    .then(data => data.data);
+const getCastInfo = async ({ movie_id, type }) => {
+  const data = await axios.get(
+    `${BASE_URL}${
+      type === 'tv' ? '/tv' : '/movie'
+    }/${movie_id}/credits?api_key=${API_KEY}&language=en-US`,
+  );
+  return data.data;
 };
-const getReviewsInfo = ({ movie_id, type, page = 1 }) => {
-  return axios
-    .get(
-      `${BASE_URL}${
-        type === 'tv' ? '/tv' : '/movie'
-      }/${movie_id}/reviews?api_key=${API_KEY}&language=en-US&page=${page}`,
-    )
-    .then(data => data.data);
+const getReviewsInfo = async ({ movie_id, type, page = 1 }) => {
+  const data = await axios.get(
+    `${BASE_URL}${
+      type === 'tv' ? '/tv' : '/movie'
+    }/${movie_id}/reviews?api_key=${API_KEY}&language=en-US&page=${page}`,
+  );
+  return data.data;
 };
 
 export {
